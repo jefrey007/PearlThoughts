3bd6c01f3d030098554570113142fdea0512fa5a
jefrey007 <95954356+jefrey007@users.noreply.github.com>
2024-06-13 13:06:28 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "hello_world_repo" {
+  name = "hello-world-repo"
+}
+
+resource "aws_ecs_cluster" "hello_world_cluster" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world_task" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world-container"
+      image     = "${aws_ecr_repository.hello_world_repo.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "hello_world_service" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.hello_world_cluster.id
+  task_definition = aws_ecs_task_definition.hello_world_task.arn
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = ["subnet-12345678", "subnet-87654321"]
+    security_groups  = ["sg-12345678"]
+    assign_public_ip = true
+  }
+}
